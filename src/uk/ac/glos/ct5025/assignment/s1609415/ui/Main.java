16e8e2c519bb5ef2914cf661dbe9608e692a5998
Joshua Walker <joshuamarkwalker@gmail.com>
2018-11-21 21:45:22 +0000

    Added Images
    added all images required in program

--- src/uk/ac/glos/ct5025/assignment/s1609415/ui/Main.java
+++ src/uk/ac/glos/ct5025/assignment/s1609415/ui/Main.java
@@ -1,4 +1,24 @@
 package uk.ac.glos.ct5025.assignment.s1609415.ui;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception{
+        Parent menuScene = FXMLLoader.load(getClass().getResource("menu.fxml"));
+        stage.setTitle("Game Suite");
+        stage.setResizable(false);
+        stage.setScene(new Scene(menuScene, 1400, 1000));
+        stage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
